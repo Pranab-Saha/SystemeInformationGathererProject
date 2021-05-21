43282fc073eaebdd72a4e528e9bb4e9816693cdd
Pranab Saha <65980817+Pranab-Saha@users.noreply.github.com>
2021-05-21 11:55:38 +0200

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -3,12 +3,7 @@ import platform
 from datetime import datetime
 
 def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
+    
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if bytes < factor:
@@ -31,7 +26,7 @@ boot_time_timestamp = psutil.boot_time()
 bt = datetime.fromtimestamp(boot_time_timestamp)
 print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
 
-# let's print CPU information
+# CPU information
 print("="*40, "CPU Info", "="*40)
 # number of cores
 print("Physical cores:", psutil.cpu_count(logical=False))
@@ -49,14 +44,14 @@ print(f"Total CPU Usage: {psutil.cpu_percent()}%")
 
 # Memory Information
 print("="*40, "Memory Information", "="*40)
-# get the memory details
+
 svmem = psutil.virtual_memory()
 print(f"Total: {get_size(svmem.total)}")
 print(f"Available: {get_size(svmem.available)}")
 print(f"Used: {get_size(svmem.used)}")
 print(f"Percentage: {svmem.percent}%")
 print("="*20, "SWAP", "="*20)
-# get the swap memory details (if exists)
+# Swap memory details (if exists)
 swap = psutil.swap_memory()
 print(f"Total: {get_size(swap.total)}")
 print(f"Free: {get_size(swap.free)}")
@@ -66,7 +61,7 @@ print(f"Percentage: {swap.percent}%")
 # Disk Information
 print("="*40, "Disk Information", "="*40)
 print("Partitions and Usage:")
-# get all disk partitions
+# Disk partitions
 partitions = psutil.disk_partitions()
 for partition in partitions:
     print(f"=== Device: {partition.device} ===")
@@ -82,14 +77,14 @@ for partition in partitions:
     print(f"  Used: {get_size(partition_usage.used)}")
     print(f"  Free: {get_size(partition_usage.free)}")
     print(f"  Percentage: {partition_usage.percent}%")
-# get IO statistics since boot
+# IO statistics since boot
 disk_io = psutil.disk_io_counters()
 print(f"Total read: {get_size(disk_io.read_bytes)}")
 print(f"Total write: {get_size(disk_io.write_bytes)}")
 
 # Network information
 print("="*40, "Network Information", "="*40)
-# get all network interfaces (virtual and physical)
+# All network interfaces 
 if_addrs = psutil.net_if_addrs()
 for interface_name, interface_addresses in if_addrs.items():
     for address in interface_addresses:
@@ -115,19 +110,12 @@ print("="*40, "GPU Details", "="*40)
 gpus = GPUtil.getGPUs()
 list_gpus = []
 for gpu in gpus:
-    # get the GPU id
     gpu_id = gpu.id
-    # name of GPU
     gpu_name = gpu.name
-    # get % percentage of GPU usage of that GPU
     gpu_load = f"{gpu.load*100}%"
-    # get free memory in MB format
     gpu_free_memory = f"{gpu.memoryFree}MB"
-    # get used memory
     gpu_used_memory = f"{gpu.memoryUsed}MB"
-    # get total memory
     gpu_total_memory = f"{gpu.memoryTotal}MB"
-    # get GPU temperature in Celsius
     gpu_temperature = f"{gpu.temperature} °C"
     gpu_uuid = gpu.uuid
     list_gpus.append((
